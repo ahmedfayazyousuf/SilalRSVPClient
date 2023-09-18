0068a44bc1d73980effce251e151aeca1bb97c93
ahmedfayazyousuf <95180025+ahmedfayazyousuf@users.noreply.github.com>
2023-09-18 13:54:59 +0400

    Changes
    Changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -38,18 +38,16 @@ const Navbar = () => {
             )
     }
     return (
-    <nav id='navigator' className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor: '#00443F'}}>   
+    <nav id='navigator' className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor: '#00443F', padding: '0', margin: '0'}} >   
         <NavLink className="navbar-brand" to="/">
-            <img style={{marginLeft:'15px', width: '47px', height: '45px', marginTop: '5px'}} src={silalwhite} alt="the hanging house logo"/>
+            <img style={{marginLeft:'15px', width: '42px', height: '40px', marginTop: '5px', marginBottom: '5px'}} src={silalwhite} alt="the hanging house logo"/>
         </NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div className="collapse navbar-collapse divvery" id="navbarSupportedContent">
             <ul className="navbar-nav ml-auto">
-
                 <RenderMenu />
-                
             </ul>
         </div>
     </nav>
